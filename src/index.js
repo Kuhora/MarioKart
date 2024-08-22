bb65e0673187d5af1bfae1f62fecf16ac99561cb
kuhora <kuhora123@gmail.com>
2024-08-22 15:14:26 -0300

    switch

--- src/index.js
+++ src/index.js
@@ -15,9 +15,39 @@ const player2 = {
 
 async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
+};
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result;
+    switch (true) {
+    case random < 0.33:
+        result = "RETA";
+        break;
+    case random < 0.66:
+        result = "CURVA";
+        break;
+    default:
+        result = "CONFRONTO";
+    } 
+    return result;
 }
+
+async function playRaceEngine(character1, character2){
+    for(let round = 1; round <=5; round++){
+        console.log(`🏁 Rodada ${round}`);
+
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
+    }
+};
+
 
 (async function main() {
     console.log (
-        "🏁🚨 Corrida entre Mario e Luigi começando...\n");
+        `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`
+);
+
+await playRaceEngine(player1, player2);
+
 })();
