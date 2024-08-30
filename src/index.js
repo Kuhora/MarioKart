396e8a54ada2d5b98b2cb463da6d920a9a38366a
kuhora <kuhora123@gmail.com>
2024-08-30 15:59:35 -0300

    loopings game

--- src/index.js
+++ src/index.js
@@ -33,13 +33,58 @@ async function getRandomBlock() {
     return result;
 }
 
+async function logRollResult(characterName, block, diceResult, attribute){
+    console.log(`${characterName}🎲 rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute}`);
+};
+
 async function playRaceEngine(character1, character2){
     for(let round = 1; round <=5; round++){
         console.log(`🏁 Rodada ${round}`);
 
+
+        // Sortear bloco
         let block = await getRandomBlock();
         console.log(`Bloco: ${block}`);
-    }
+
+            //rolar os dados
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+
+    //teste de habilidade
+    let TotalTestSkill1 = 0;
+    let TotalTestSkill2= 0;
+
+    if(block === "RETA"){
+        TotalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+        TotalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+
+        await logRollResult(character1.NOME, "velocidade", diceResult1, character1.VELOCIDADE);
+        await logRollResult(character2.NOME, "velocidade", diceResult1, character2.VELOCIDADE);
+    };
+    if(block === "CURVA"){
+        TotalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+        TotalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+        
+        await logRollResult(character1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
+        await logRollResult(character2.NOME, "manobrabilidade", diceResult1, character2.MANOBRABILIDADE);
+    };
+    if(block === "CONFRONTO"){
+        let powerResult1 = diceResult1 + character1.PODER;
+        let powerResult2 = diceResult2 + character2.PODER;
+        };
+
+
+        // verificando o vencedor
+        if(TotalTestSkill1 > TotalTestSkill2){
+            console.log(`${character1.NOME} marcou um ponto!`);
+            character1.PONTOS++;
+        } else if(TotalTestSkill2 > TotalTestSkill1){
+            console.log(`${character2.NOME} marcou um ponto!`);
+            character2.PONTOS++;
+        }
+        console.log("-----------------------");
+    };
 };
 
 
